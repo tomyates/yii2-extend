2683e4d66cb0b22619dc6ac3bc1a717a8ac8d54f
Matt Chadwick <matt@tariffstreet.com>
2016-06-24 15:58:49 +0100

    Include security.php inc generateRandomAlphanumericString()

---
+++ Security.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace common\components;
+
+class Security extends \yii\base\Security
+{
+	public function generateRandomAlphanumericString($length = 32)
+	{
+		if (!is_int($length)) {
+            throw new InvalidParamException('First parameter ($length) must be an integer');
+        }
+        if ($length < 1) {
+            throw new InvalidParamException('First parameter ($length) must be greater than 0');
+        }
+        
+        $string = '';
+        
+        while(strlen($string) < $length)
+        {
+	        $newPartOfString = self::generateRandomString($length - strlen($string));
+	        
+	        //Remove all dashes and underscores
+	        $newPartOfString = str_replace(['-', '_'], '', $newPartOfString);
+	        
+	        $string .= $newPartOfString;
+        }
+        
+        return $string;
+	}
+}
